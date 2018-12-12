659c8d9e595974c6a3bfcb765906329bb82b006d
Yanuar <45526227+Yanuarramadhan@users.noreply.github.com>
2018-12-12 18:24:58 +0700

    codingan mencari luas lingkaran

---
+++ soal2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int r;
+    float phi=3.14,luas;
+
+    cout<<"masukan jari jari lingkaran tsb:";
+    cin>>r;
+
+    luas=phi*r*r;
+    cout<<"jadi luas lingkarannya yaitu"<<luas;
+    return 0;
+}
